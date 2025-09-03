c97d152853d1a181f47f89a278a55be9fed6024b
Darwin <jackdarwinbot@gmail.com>
2025-09-03 11:30:31 -0600

    Segundo Comit Trabajando con interfaz Grafica

--- Parte_grafica.py
+++ Parte_grafica.py
@@ -3,37 +3,13 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.title("Mi primera ventana")
 ventana.geometry("300x200")
-
-etiqueta = tk.Label(ventana, text="Escribe tu nombre:")
-etiqueta.pack(pady=5)
-
-entrada = tk.Entry(ventana)
-entrada.pack(pady=5)
-
-def saludar():
-    nombre = entrada.get()
-    etiqueta.config(text=f"Hola, {nombre}!")
-
 def limpiar():
-    entrada.delete(0, tk.END)
-    etiqueta.config(text="Escribe tu nombre:")
+
     entrada1.delete(0, tk.END)
     entrada2.delete(0, tk.END)
 
-
-
-
-boton_saludar = tk.Button(ventana, text="Saludar", command=saludar)
-boton_saludar.pack(pady=5)
-
-boton_limpiar = tk.Button(ventana, text="Limpiar", command=limpiar)
-boton_limpiar.pack(pady=5)
-
-boton_salir = tk.Button(ventana, text="Salir", command=ventana.quit)
-boton_salir.pack(pady=5)
-
-etiqueta_resutado = tk.Label(ventana, text="el resultado es:")
-etiqueta_resutado.pack(pady=5)
+etiqueta_titulo = tk.Label(ventana, text="CALCULADORA:")
+etiqueta_titulo.pack(pady=5)
 
 etiqueta1 = tk.Label(ventana, text="Escribe El primer numero:")
 etiqueta1.pack(pady=5)
@@ -41,7 +17,7 @@ etiqueta1.pack(pady=5)
 entrada1 = tk.Entry(ventana)
 entrada1.pack(pady=5)
 
-etiqueta2 = tk.Label(ventana, text="Escribe el Segundo mumero:")
+etiqueta2 = tk.Label(ventana, text="Escribe el Segundo numero:")
 etiqueta2.pack(pady=5)
 
 entrada2 = tk.Entry(ventana)
@@ -83,6 +59,11 @@ def dividir():
     except ValueError:
         etiqueta_resutado.config(text="Por favor, ingresa números válidos.")
 
+def limpiar():
+
+    entrada1.delete(0, tk.END)
+    entrada2.delete(0, tk.END)
+
 boton_sumar = tk.Button(ventana, text="Sumar", command=sumar)
 boton_sumar.pack(pady=5)
 
@@ -95,5 +76,13 @@ boton_multiplicar.pack(pady=5)
 boton_dividir = tk.Button(ventana, text="Dividir", command=dividir)
 boton_dividir.pack(pady=5)
 
+etiqueta_resutado = tk.Label(ventana, text="EL RESULTADO DE LA OPERACION ES:")
+etiqueta_resutado.pack(pady=5)
+
+boton_limpiar = tk.Button(ventana, text="Limpiar", command=limpiar)
+boton_limpiar.pack(pady=5)
+
+boton_salir = tk.Button(ventana, text="Salir", command=ventana.quit)
+boton_salir.pack(pady=5)
 
 ventana.mainloop()
