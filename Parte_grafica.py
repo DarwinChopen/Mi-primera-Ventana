829feb5690e1baacbd7489ce9f12a548c10d26dd
Darwin <jackdarwinbot@gmail.com>
2025-09-03 21:17:22 -0600

    Cuarto Comit, Mejoramiento visual

--- Parte_grafica.py
+++ Parte_grafica.py
@@ -4,7 +4,7 @@ ventana = tk.Tk()
 ventana.title("Mi primera ventana")
 ventana.geometry("400x400+500+300")
 ventana.configure(bg="grey")
-
+ventana.resizable(False, False)
 etiqueta_titulo = tk.Label(
     ventana,
     text="CALCULADORA",
@@ -44,7 +44,7 @@ def sumar():
         num1 = int(entrada1.get())
         num2 = int(entrada2.get())
         resultado = num1 + num2
-        etiqueta_resutado.config(text=f"El resultado es: {resultado}")
+        etiqueta_resutado.config(text=f"Suma:{resultado}")
     except ValueError:
         etiqueta_resutado.config(text="Por favor, ingresa números válidos.")
 
@@ -53,7 +53,7 @@ def restar():
         num1 = int(entrada1.get())
         num2 = int(entrada2.get())
         resultado = num1 - num2
-        etiqueta_resutado.config(text=f"El resultado es: {resultado}")
+        etiqueta_resutado.config(text=f"Resta:{resultado}")
     except ValueError:
         etiqueta_resutado.config(text="Por favor, ingresa números válidos.")
 
@@ -62,7 +62,7 @@ def multiplicar():
         num1 = int(entrada1.get())
         num2 = int(entrada2.get())
         resultado = num1 * num2
-        etiqueta_resutado.config(text=f"El resultado es: {resultado}")
+        etiqueta_resutado.config(text=f"Multiplicacion: {resultado}")
     except ValueError:
         etiqueta_resutado.config(text="Por favor, ingresa números válidos.")
 
@@ -71,7 +71,7 @@ def dividir():
         num1 = int(entrada1.get())
         num2 = int(entrada2.get())
         resultado = num1 / num2
-        etiqueta_resutado.config(text=f"El resultado es: {resultado}")
+        etiqueta_resutado.config(text=f"Division: {resultado}")
     except ValueError:
         etiqueta_resutado.config(text="Por favor, ingresa números válidos.")
 
@@ -79,24 +79,24 @@ def limpiar():
 
     entrada1.delete(0, tk.END)
     entrada2.delete(0, tk.END)
-    etiqueta_resutado.config(text="El Resultado de la operacion es: ")
+    etiqueta_resutado.config(text="")
 
 frame_botones = tk.Frame(ventana, bg="grey",)
 frame_botones.pack(pady=5)
 
-boton_sumar = tk.Button(frame_botones, text="Sumar", command=sumar)
+boton_sumar = tk.Button(frame_botones, text="+",width=6, height=2, command=sumar)
 boton_sumar.pack(side="left",padx=5)
 
-boton_restar = tk.Button(frame_botones, text="Restar", command=restar)
+boton_restar = tk.Button(frame_botones, text="-",width=6, height=2, command=restar)
 boton_restar.pack(side="left",padx=5)
 
-boton_multiplicar = tk.Button(frame_botones, text="Multiplicar", command=multiplicar)
+boton_multiplicar = tk.Button(frame_botones, text="*",width=6, height=2, command=multiplicar)
 boton_multiplicar.pack(side="left",padx=5)
 
-boton_dividir = tk.Button(frame_botones, text="Dividir", command=dividir)
+boton_dividir = tk.Button(frame_botones, text="/",width=6, height=2, command=dividir)
 boton_dividir.pack(side="left",padx=5)
 
-etiqueta_resutado = tk.Label(ventana, text="EL RESULTADO DE LA OPERACION ES:")
+etiqueta_resutado = tk.Label(ventana, text="00")
 etiqueta_resutado.pack(pady=10)
 
 frame_lisa = tk.Frame(ventana, bg="grey")
